eb24ca67d26644f1f873eff6e859e16125c6c508
Oleg Filippov <enternetacum@yandex.ru>
2016-09-08 15:32:22 +0300

    Revert "Remove local server Copy"
    This reverts commit 6879e6a67308d0ef5f80b9139fc75e2e98f5d87e.

---
+++ src/server/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port: 8080,
+    bots: 0, // Not Implemented
+    maxConnections: 100,
+	highscoreName: "How to change Highscore Msg",
+	highscoreMsg: "Set this message in the config",
+    food: 20000,
+    foodColours: 8,
+    foodSize: [15, 47],
+    foodPerSpawn: 1000, // Not Implemented
+    gameRadius: 21600,
+    sectorSize: 480
+};
